86344dbc54656b96adcee6dd1c4f312e59c45fa7
Artem Chivchalov <chivchalov@gmail.com>
2016-11-09 11:14:07 +0300

    initial import

---
+++ index.js
@@ -0,0 +1,140 @@
+const dnode = require('dnode');
+const q = require('q');
+const net = require('net');
+const path = require('path');
+const _ = require('lodash');
+
+exports.configManager = require('./lib/config-manager');
+exports.storage = require('./lib/storage');
+
+exports.findPort = function findPort(port) {
+    var defer = q.defer();
+    var server = net.createServer(socket => socket.end());
+    server.listen(port, function (err) {
+        server.once('close', function () {
+            defer.resolve(port);
+        });
+        server.close();
+    });
+    server.on('error', function (err) {
+        defer.resolve(findPort(port+1));
+    });
+    return defer.promise;
+};
+
+exports.encodeTerrain = function(terrain) {
+    var result = '';
+    for(var y=0; y<50; y++) {
+        for(var x=0; x<50; x++) {
+            var objects = _.filter(terrain, {x,y}),
+            code = 0;
+            if(_.any(objects, {type: 'wall'})) {
+                code = code | 1;
+            }
+            if(_.any(objects, {type: 'swamp'})) {
+                code = code | 2;
+            }
+            result = result + code;
+        }
+    }
+    return result;
+};
+
+exports.decodeTerrain = function(str, room) {
+    var result = [];
+
+    for(var y=0; y<50; y++) {
+        for(var x=0; x<50; x++) {
+            var code = str.charAt(y*50+x);
+            if(code & 1) {
+                result.push({room, x, y, type: 'wall'});
+            }
+            if(code & 2) {
+                result.push({room, x, y, type: 'swamp'});
+            }
+        }
+    }
+
+    return result;
+};
+
+exports.checkTerrain = function(terrain, x, y, mask) {
+    return (parseInt(terrain.charAt(y*50 + x)) & mask) > 0;
+};
+
+exports.getGametime = function() {
+    return exports.storage.env.get(exports.storage.env.keys.GAMETIME).then(data => parseInt(data));
+};
+
+exports.getDiff = function(oldData, newData) {
+
+    function getIndex(data) {
+        var index = {};
+        _.forEach(data, (obj) => index[obj._id] = obj);
+        return index;
+    }
+
+
+    var result = {},
+    oldIndex = getIndex(oldData),
+    newIndex = getIndex(newData);
+
+    _.forEach(oldData, (obj) => {
+        if(newIndex[obj._id]) {
+            var newObj = newIndex[obj._id];
+            var objDiff = result[obj._id] = {};
+            for(var key in obj) {
+                if(_.isUndefined(newObj[key])) {
+                    objDiff[key] = null;
+                }
+                else if((typeof obj[key]) != (typeof newObj[key]) || obj[key] && !newObj[key]) {
+                    objDiff[key] = newObj[key];
+                }
+                else if(_.isObject(obj[key])) {
+
+                    objDiff[key] = {};
+
+                    for (var subkey in obj[key]) {
+                        if (!_.isEqual(obj[key][subkey], newObj[key][subkey])) {
+                            objDiff[key][subkey] = newObj[key][subkey];
+                        }
+                    }
+                    for (var subkey in newObj[key]) {
+                        if (_.isUndefined(obj[key][subkey])) {
+                            objDiff[key][subkey] = newObj[key][subkey];
+                        }
+                    }
+                    if (!_.size(objDiff[key])) {
+                        delete result[obj._id][key];
+                    }
+                }
+                else if(!_.isEqual(obj[key], newObj[key])) {
+                    objDiff[key] = newObj[key];
+                }
+            }
+            for(var key in newObj) {
+                if(_.isUndefined(obj[key])) {
+                    objDiff[key] = newObj[key];
+                }
+            }
+            if(!_.size(objDiff)) {
+                delete result[obj._id];
+            }
+        }
+        else {
+            result[obj._id] = null;
+        }
+    });
+
+    _.forEach(newData, (obj) => {
+        if(!oldIndex[obj._id]) {
+            result[obj._id] = obj;
+        }
+    });
+
+    return result;
+};
+
+exports.qSequence = function(collection, fn) {
+    return _.reduce(collection, (promise, element, key) => promise.then(() => fn(element,key)), q.when());
+};
