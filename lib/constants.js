screeps/common

commit a6b94fda97753fce13e4a7216b50e746b65468ed

feat(constants): support for `Room.eventLog`

lib/constants.js
@@ -101,7 +101,7 @@ module.exports = {
     WORLD_HEIGHT: 202,
 
     CREEP_LIFE_TIME: 1500,
-    CREEP_CLAIM_LIFE_TIME: 600,
+    CREEP_CLAIM_LIFE_TIME: 500,
     CREEP_CORPSE_RATE: 0.2,
 
     CARRY_CAPACITY: 50,
@@ -260,7 +260,7 @@ module.exports = {
     LAB_ENERGY_CAPACITY: 2000,
     LAB_BOOST_ENERGY: 20,
     LAB_BOOST_MINERAL: 30,
-    LAB_COOLDOWN: 10,               // not used
+    LAB_COOLDOWN: 10,
     LAB_REACTION_AMOUNT: 5,
 
     GCL_POW: 2.4,
@@ -690,7 +690,28 @@ module.exports = {
     SYSTEM_USERNAME: 'Screeps',
 
     SIGN_NOVICE_AREA: 'A new Novice Area is being planned somewhere in this sector. Please make sure all important rooms are reserved.',
-    SIGN_RESPAWN_AREA: 'A new Respawn Area is being planned somewhere in this sector. Please make sure all important rooms are reserved.'
+    SIGN_RESPAWN_AREA: 'A new Respawn Area is being planned somewhere in this sector. Please make sure all important rooms are reserved.',
+
+    EVENT_ATTACK: 1,
+    EVENT_OBJECT_DESTROYED: 2,
+    EVENT_ATTACK_CONTROLLER: 3,
+    EVENT_BUILD: 4,
+    EVENT_HARVEST: 5,
+    EVENT_HEAL: 6,
+    EVENT_REPAIR: 7,
+    EVENT_RESERVE_CONTROLLER: 8,
+    EVENT_UPGRADE_CONTROLLER: 9,
+    EVENT_EXIT: 10,
+
+    EVENT_ATTACK_TYPE_MELEE: 1,
+    EVENT_ATTACK_TYPE_RANGED: 2,
+    EVENT_ATTACK_TYPE_RANGED_MASS: 3,
+    EVENT_ATTACK_TYPE_DISMANTLE: 4,
+    EVENT_ATTACK_TYPE_HIT_BACK: 5,
+    EVENT_ATTACK_TYPE_NUKE: 6,
+
+    EVENT_HEAL_TYPE_MELEE: 1,
+    EVENT_HEAL_TYPE_RANGED: 2,
 };
 
 

commit 4435e99a07e699560214d6addea2fe4545b68b1f

hotfix(constants): fix merge conflict regression from commit a6b94fda97753fce13e4a7216b50e746b65468ed

lib/constants.js
@@ -101,7 +101,7 @@ module.exports = {
     WORLD_HEIGHT: 202,
 
     CREEP_LIFE_TIME: 1500,
-    CREEP_CLAIM_LIFE_TIME: 500,
+    CREEP_CLAIM_LIFE_TIME: 600,
     CREEP_CORPSE_RATE: 0.2,
     CREEP_PART_MAX_ENERGY: 125,
 
@@ -262,7 +262,7 @@ module.exports = {
     LAB_ENERGY_CAPACITY: 2000,
     LAB_BOOST_ENERGY: 20,
     LAB_BOOST_MINERAL: 30,
-    LAB_COOLDOWN: 10,
+    LAB_COOLDOWN: 10,           // not used
     LAB_REACTION_AMOUNT: 5,
     LAB_UNBOOST_ENERGY: 0,
     LAB_UNBOOST_MINERAL: 15,